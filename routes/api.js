6e463a62046d00d3ab9e65c2aecc12ff7b636c1b
Tyler <realpsych22@gmail.com>
2020-04-13 11:07:59 -0500

    Message posts work but do not redirect

--- routes/api.js
+++ routes/api.js
@@ -9,10 +9,49 @@
 'use strict';
 
 var expect = require('chai').expect;
+var MongoClient = require('mongodb');
+var ObjectId = require('mongodb').ObjectID;
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var Schema = mongoose.Schema;
+
+var messageSchema = new Schema({
+  text: String,
+  created_on: Date,
+  bumped_on: Date,
+  reported: { type: Boolean, default: false },
+  delete_password: String,
+  replies: [String]
+});
+
+var Message = mongoose.model("Message", messageSchema);
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+  .post(function (req, res){
+      var board = req.params.board;
+      var inputs = req.body;
+       if(!inputs.text||!inputs.delete_password){
+      res.send("please fill out required fields")
+    }
+    else{
+      var message=new Message({
+  text: inputs.text,
+  created_on: Date.now(),
+  bumped_on: Date.now(),
+  reported: false,
+  delete_password: inputs.delete_password,
+  replies:  []
+    })
+      console.log(message)
+  message.save;
+  //    res.redirect('/b/'+inputs.board)
+      res.send(message)
+  }
+  })
     
   app.route('/api/replies/:board');
 
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -18,8 +18,26 @@ suite('Functional Tests', function() {
   suite('API ROUTING FOR /api/threads/:board', function() {
     
     suite('POST', function() {
-      
-    });
+            test('Every field filled in', function(done) {
+       chai.request(server)
+        .post('/api/threads/test')
+        .send({
+          text: 'this is some text',
+          delete_password: 'supersecretpassword',
+        })
+        .end(function(err, res){
+  //       expect(res).to.redirectTo('/api/threads/b/test')
+          assert.property(res.body, "text", "output needs text field")
+          assert.property(res.body, "created_on", "output needs created_on")
+          assert.property(res.body, "bumped_on", "output needs bumped_on")
+          assert.property(res.body, "reported", "output needs reported")
+          assert.property(res.body, "delete_password", "output needs delete_password")
+          assert.property(res.body, "replies", "output needs replies")
+        })
+          done();
+                }) 
+        });
+   
     
     suite('GET', function() {
       
